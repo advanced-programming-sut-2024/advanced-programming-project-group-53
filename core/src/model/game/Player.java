75134ba79767a9066eac873c6767c1b59e129207
Safar <safariamirparsa@gmail.com>
2024-06-03 12:48:54 +0330

    player updated

--- core/src/model/game/Player.java
+++ core/src/model/game/Player.java
@@ -2,24 +2,32 @@ package model.game;
 
 import model.card.Card;
 import model.card.Faction;
+import model.cards.AllCard;
+import model.cards.Deck;
+import model.cards.DiscardPile;
+import model.cards.Hand;
 
 import java.util.ArrayList;
 
 public class Player {
-    private User user;
-    private ArrayList<Card> deck;
-    private Round round;
+    private final User user;
+    private final Deck deck;
+    private final Hand hand;
+    private final DiscardPile discardPile;
+    private final AllCard allCard;
     private final Faction faction;
     private int point;
-
-    public Player(Faction faction) {
+    //remember to check if commander card is needed or not
+    public Player(User user, Deck deck, Hand hand, AllCard allCard, Faction faction) {
+        //TODO: check allCard configuration and application.
+        this.user = user;
+        this.deck = deck;
+        this.allCard = allCard;
+        this.hand = hand;
+        this.discardPile = new DiscardPile();
         this.faction = faction;
     }
 
-    //remember to check if commander card is needed or not
-    public void setFaction(Faction faction){
-
-    }
     public void addToDeck(Card card) {
 
     }
@@ -27,12 +35,32 @@ public class Player {
 
     }
 
+
     public User getUser() {
         return user;
     }
 
+    public void setPoint(int point) {
+        this.point = point;
+    }
     public int getPoint() {
         return point;
+    }
+
+    public Deck getDeck() {
+        return deck;
+    }
+
+    public Hand getHand() {
+        return hand;
+    }
+
+    public DiscardPile getDiscardPile() {
+        return discardPile;
+    }
+
+    public AllCard getAllCard() {
+        return allCard;
     }
 
     public Faction getFaction() {
