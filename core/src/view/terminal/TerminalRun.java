6f279d6b504c4b1f4bdea37e8b0219940055915e
Safar <safariamirparsa@gmail.com>
2024-06-04 11:17:24 +0330

    terminal inputs completed.

--- core/src/view/terminal/TerminalRun.java
+++ core/src/view/terminal/TerminalRun.java
@@ -13,7 +13,7 @@ public abstract class TerminalRun {
     private static Menu currentMenu = LoginMenu.getInstance();
 
     public static void loginMenuRun(Scanner scanner) {
-        while(true) {
+        while (true) {
             String line = scanner.nextLine();
             checkGeneralCommands(line, scanner);
             Matcher matcher = LoginMenuRegex.skip.getMatcher(line);
@@ -35,67 +35,214 @@ public abstract class TerminalRun {
 
 
     public static void registerMenuRun(Scanner scanner) {
-        String line = scanner.nextLine();
-        checkGeneralCommands(line, scanner);
-        Matcher matcher = RegisterMenuRegex.register.getMatcher(line);
-        if (matcher.find()) {
-            //TODO : fill this with controller.
+        while (true) {
+            String line = scanner.nextLine();
+            checkGeneralCommands(line, scanner);
+            Matcher matcher = RegisterMenuRegex.register.getMatcher(line);
+            if (matcher.find()) {
+                //TODO : fill this with controller.
+            }
+            matcher = RegisterMenuRegex.pickQuestion.getMatcher(line);
+            if (matcher.find()) {
+                //TODO : fill this with controller.
+            } else Printer.print(MenuMessage.INVALID_COMMAND.message());
         }
-        matcher = RegisterMenuRegex.pickQuestion.getMatcher(line);
-        if (matcher.find()) {
-            //TODO : fill this with controller.
-        } else Printer.print(MenuMessage.INVALID_COMMAND.message());
     }
 
     public static void mainMenuRun(Scanner scanner) {
-        String line = scanner.nextLine();
-        checkGeneralCommands(line, scanner);
-        Matcher matcher = MainMenuRegex.logout.getMatcher(line);
-        if (matcher.find()) {
-            //TODO : fill this with controller.
+        while (true) {
+            String line = scanner.nextLine();
+            checkGeneralCommands(line, scanner);
+            Matcher matcher = MainMenuRegex.logout.getMatcher(line);
+            if (matcher.find()) {
+                //TODO : fill this with controller.
+            }
+            matcher = MainMenuRegex.changeUsername.getMatcher(line);
+            if (matcher.find()) {
+                //TODO : fill this with controller.
+            }
+            matcher = MainMenuRegex.changePassword.getMatcher(line);
+            if (matcher.find()) {
+                //TODO : fill this with controller.
+            }
+            matcher = MainMenuRegex.changeNickname.getMatcher(line);
+            if (matcher.find()) {
+                //TODO : fill this with controller.
+            }
+            matcher = MainMenuRegex.changeEmail.getMatcher(line);
+            if (matcher.find()) {
+                //TODO : fill this with controller.
+            } else Printer.print(MenuMessage.INVALID_COMMAND.message());
         }
-        matcher = MainMenuRegex.changeUsername.getMatcher(line);
-        if (matcher.find()) {
-            //TODO : fill this with controller.
-        }
-        matcher = MainMenuRegex.changePassword.getMatcher(line);
-        if (matcher.find()) {
-            //TODO : fill this with controller.
-        }
-        matcher = MainMenuRegex.changeNickname.getMatcher(line);
-        if (matcher.find()) {
-            //TODO : fill this with controller.
-        }
-        matcher = MainMenuRegex.changeEmail.getMatcher(line);
-        if (matcher.find()) {
-            //TODO : fill this with controller.
-        } else Printer.print(MenuMessage.INVALID_COMMAND.message());
     }
 
     public static void profileMenuRun(Scanner scanner) {
-        String line = scanner.nextLine();
-        checkGeneralCommands(line, scanner);
-        Matcher matcher = ProfileMenuRegex.enterUserInfo.getMatcher(line);
-        if (matcher.find()) {
-            //TODO : fill this with controller.
+        while (true) {
+            String line = scanner.nextLine();
+            checkGeneralCommands(line, scanner);
+            Matcher matcher = ProfileMenuRegex.enterUserInfo.getMatcher(line);
+            if (matcher.find()) {
+                //TODO : fill this with controller.
+            }
+            matcher = ProfileMenuRegex.gameHistoryNormal.getMatcher(line);
+            if (matcher.find()) {
+                //TODO : fill this with controller.
+            }
+            matcher = ProfileMenuRegex.gameHistorySpecified.getMatcher(line);
+            if (matcher.find()) {
+                //TODO : fill this with controller.
+            } else Printer.print(MenuMessage.INVALID_COMMAND.message());
         }
-        matcher = ProfileMenuRegex.gameHistoryNormal.getMatcher(line);
-        if (matcher.find()) {
-            //TODO : fill this with controller.
-        }
-        matcher = ProfileMenuRegex.gameHistorySpecified.getMatcher(line);
-        if (matcher.find()) {
-            //TODO : fill this with controller.
-        } else Printer.print(MenuMessage.INVALID_COMMAND.message());
     }
 
     public static void startMenuRun(Scanner scanner) {
+        while (true) {
+            String line = scanner.nextLine();
+            checkGeneralCommands(line, scanner);
+            Matcher matcher = StartMenuRegex.createGame.getMatcher(line);
+            if (matcher.find()) {
+                //TODO : fill this with controller.
+            }
+            matcher = StartMenuRegex.showFactions.getMatcher(line);
+            if (matcher.find()) {
+                //TODO : fill this with controller.
+            }
+            matcher = StartMenuRegex.selectFaction.getMatcher(line);
+            if (matcher.find()) {
+                //TODO : fill this with controller.
+            }
+            matcher = StartMenuRegex.showCards.getMatcher(line);
+            if (matcher.find()) {
+                //TODO : fill this with controller.
+            }
+            matcher = StartMenuRegex.showDeck.getMatcher(line);
+            if (matcher.find()) {
+                //TODO : fill this with controller.
+            }
+            matcher = StartMenuRegex.showCurrentUserInformation.getMatcher(line);
+            if (matcher.find()) {
+                //TODO : fill this with controller.
+            }
+            matcher = StartMenuRegex.showLeaders.getMatcher(line);
+            if (matcher.find()) {
+                //TODO : fill this with controller.
+            }
+            matcher = StartMenuRegex.startGame.getMatcher(line);
+            if (matcher.find()) {
+                //TODO : fill this with controller.
+            }
+            matcher = StartMenuRegex.changeTurn.getMatcher(line);
+            if (matcher.find()) {
+                //TODO : fill this with controller.
+            }
+            matcher = StartMenuRegex.saveDeckAddress.getMatcher(line);
+            if (matcher.find()) {
+                //TODO : fill this with controller.
+            }
+            matcher = StartMenuRegex.saveDeckName.getMatcher(line);
+            if (matcher.find()) {
+                //TODO : fill this with controller.
+            }
+            matcher = StartMenuRegex.loadDeckAddress.getMatcher(line);
+            if (matcher.find()) {
+                //TODO : fill this with controller.
+            }
+            matcher = StartMenuRegex.loadDeckName.getMatcher(line);
+            if (matcher.find()) {
+                //TODO : fill this with controller.
+            }
+            matcher = StartMenuRegex.selectLeader.getMatcher(line);
+            if (matcher.find()) {
+                //TODO : fill this with controller.
+            }
+            matcher = StartMenuRegex.addToDeck.getMatcher(line);
+            if (matcher.find()) {
+                //TODO : fill this with controller.
+            }
+            matcher = StartMenuRegex.deleteFromDeck.getMatcher(line);
+            if (matcher.find()) {
+                //TODO : fill this with controller.
+            } else Printer.print(MenuMessage.INVALID_COMMAND.message());
+        }
 
     }
 
     public static void gameMenuRun(Scanner scanner) {
-
+        while (true) {
+            String line = scanner.nextLine();
+            checkGeneralCommands(line, scanner);
+            Matcher matcher = GameMenuRegex.veto.getMatcher(line);
+            if (matcher.find()) {
+                //TODO : fill this with controller.
+            }
+            matcher = GameMenuRegex.inHandDeck.getMatcher(line);
+            if (matcher.find()) {
+                //TODO : fill this with controller.
+            }
+            matcher = GameMenuRegex.remainingCards.getMatcher(line);
+            if (matcher.find()) {
+                //TODO : fill this with controller.
+            }
+            matcher = GameMenuRegex.outOfPlayCards.getMatcher(line);
+            if (matcher.find()) {
+                //TODO : fill this with controller.
+            }
+            matcher = GameMenuRegex.cardsInRow.getMatcher(line);
+            if (matcher.find()) {
+                //TODO : fill this with controller.
+            }
+            matcher = GameMenuRegex.spellsInPlay.getMatcher(line);
+            if (matcher.find()) {
+                //TODO : fill this with controller.
+            }
+            matcher = GameMenuRegex.placeCard.getMatcher(line);
+            if (matcher.find()) {
+                //TODO : fill this with controller.
+            }
+            matcher = GameMenuRegex.showCommander.getMatcher(line);
+            if (matcher.find()) {
+                //TODO : fill this with controller.
+            }
+            matcher = GameMenuRegex.commanderPowerPlay.getMatcher(line);
+            if (matcher.find()) {
+                //TODO : fill this with controller.
+            }
+            matcher = GameMenuRegex.showPlayersInfo.getMatcher(line);
+            if (matcher.find()) {
+                //TODO : fill this with controller.
+            }
+            matcher = GameMenuRegex.showNumberOfCardsInHand.getMatcher(line);
+            if (matcher.find()) {
+                //TODO : fill this with controller.
+            }
+            matcher = GameMenuRegex.showPlayerLives.getMatcher(line);
+            if (matcher.find()) {
+                //TODO : fill this with controller.
+            }
+            matcher = GameMenuRegex.showTurnInfo.getMatcher(line);
+            if (matcher.find()) {
+                //TODO : fill this with controller.
+            }
+            matcher = GameMenuRegex.showTotalScore.getMatcher(line);
+            if (matcher.find()) {
+                //TODO : fill this with controller.
+            }
+            matcher = GameMenuRegex.passRound.getMatcher(line);
+            if (matcher.find()) {
+                //TODO : fill this with controller.
+            }
+            matcher = GameMenuRegex.passRound.getMatcher(line);
+            if (matcher.find()) {
+                //TODO : fill this with controller.
+            }
+            matcher = GameMenuRegex.showTotalScoreOfRow.getMatcher(line);
+            if (matcher.find()) {
+                //TODO : fill this with controller.
+            } else Printer.print(MenuMessage.INVALID_COMMAND.message());
+        }
     }
+
+
     public static void changeCurrentMenu(Menu menu) {
         currentMenu = menu;
     }
