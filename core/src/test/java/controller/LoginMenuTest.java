0cbd070d1960456cb9a70b1cd5292c6fbe992de5
farbodft <farbod.ft2005@gmail.com>
2024-06-10 23:21:00 +0330

    setup for login menu (before testing) added

--- core/src/test/java/controller/LoginMenuTest.java
+++ core/src/test/java/controller/LoginMenuTest.java
@@ -1,7 +1,16 @@
 package controller;
 
+import jdk.jpackage.internal.Log;
+import org.junit.Before;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class LoginMenuTest {
+    private LoginMenu loginMenu;
+
+    @Before
+    public void setUp() {
+        loginMenu = LoginMenu.getInstance();
+    }
 
 }
