40ecbfd6932dce56d01db253c6a2cc7c3a4d59fe
farbodft <farbod.ft2005@gmail.com>
2024-06-11 20:34:45 +0330

    setUp method updated

--- core/src/test/java/controller/LoginMenuTest.java
+++ core/src/test/java/controller/LoginMenuTest.java
@@ -1,5 +1,6 @@
 package controller;
 
+import model.game.User;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -8,13 +9,16 @@ import java.io.PrintStream;
 
 import static org.junit.Assert.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.*;
 
 public class LoginMenuTest {
     private LoginMenu loginMenu;
+    private User mockUser;
 
     @Before
     public void setUp() {
         loginMenu = LoginMenu.getInstance();
+        mockUser = mock(User.class);
     }
 
     @Test
@@ -27,6 +31,8 @@ public class LoginMenuTest {
         ByteArrayOutputStream outContent = new ByteArrayOutputStream();
         System.setOut(new PrintStream(outContent));
         loginMenu.showMenu();
-        assertEquals("LoginMenu",outContent.toString().trim());
+        assertEquals("LoginMenu", outContent.toString().trim());
     }
+
+
 }
