bc68aaa6da61992321ab0f5dad58987b1a5861c8
farbodft <farbod.ft2005@gmail.com>
2024-06-17 23:15:40 +0330

    SetUp method for RegisterMenuTest created

--- core/src/test/java/controller/RegisterMenuTest.java
+++ core/src/test/java/controller/RegisterMenuTest.java
@@ -1,9 +1,20 @@
 package controller;
 
 import junit.framework.TestCase;
+import model.game.User;
+import org.junit.Before;
+
+import java.util.ArrayList;
 
 public class RegisterMenuTest extends TestCase {
     private RegisterMenu registerMenu;
+    ArrayList<User> allUsersTemp = new ArrayList<>();
 
+    @Before
+    public void setUp() {
+        registerMenu = RegisterMenu.getInstance();
+        User.resetUsers(allUsersTemp);
+        User userInstance = new User("aValidUsername-1","aValid-1Nickname","valid2mail@gmail.com","Valid#Strong45password");
+    }
 
 }
