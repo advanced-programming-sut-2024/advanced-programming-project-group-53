76957a390899ebbb2a940f0ed8bdd55fb27f9aa5
farbodft <farbod.ft2005@gmail.com>
2024-06-19 00:14:59 +0330

    Before setup method created for MainMenuTest

--- core/src/test/java/controller/MainMenuTest.java
+++ core/src/test/java/controller/MainMenuTest.java
@@ -1,12 +1,22 @@
 package controller;
 
+import model.game.User;
+import org.junit.Before;
+
 import java.io.ByteArrayOutputStream;
+import java.util.ArrayList;
 
 import static org.junit.Assert.*;
 
 public class MainMenuTest {
     private MainMenu mainMenu;
+    private static final ArrayList<User> allUsersTemp = new ArrayList<>();
     ByteArrayOutputStream outContent = new ByteArrayOutputStream();
 
-
+    @Before
+    public void setUp() {
+        mainMenu = MainMenu.getInstance();
+        User.resetUsers(allUsersTemp);
+        User userInstance = new User("aValidUsername-1", "aValid-1Nickname", "valid2mail@gmail.com", "Valid#Strong45password");
+    }
 }
