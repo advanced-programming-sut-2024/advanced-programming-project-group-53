85f08b418077c8258bfb231de4caa09b00a3a018
farbodft <farbod.ft2005@gmail.com>
2024-06-19 00:19:38 +0330

    should show main menu test method added

--- core/src/test/java/controller/MainMenuTest.java
+++ core/src/test/java/controller/MainMenuTest.java
@@ -2,8 +2,11 @@ package controller;
 
 import model.game.User;
 import org.junit.Before;
+import org.junit.Test;
+import view.terminal.Message.MenuMessage;
 
 import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 import java.util.ArrayList;
 
 import static org.junit.Assert.*;
@@ -18,5 +21,12 @@ public class MainMenuTest {
         mainMenu = MainMenu.getInstance();
         User.resetUsers(allUsersTemp);
         User userInstance = new User("aValidUsername-1", "aValid-1Nickname", "valid2mail@gmail.com", "Valid#Strong45password");
+    }
+
+    @Test
+    public void shouldShowMainMenu() {
+        System.setOut(new PrintStream(outContent));
+        mainMenu.showMenu();
+        assertEquals(MenuMessage.MAIN_MENU.message(), outContent.toString().trim());
     }
 }
