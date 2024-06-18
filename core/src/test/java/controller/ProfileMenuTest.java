3478e7a9650122c502aa5a3f9f3da52c4e9698f6
farbodft <farbod.ft2005@gmail.com>
2024-06-19 01:14:40 +0330

    test file for profile menu created

---
+++ core/src/test/java/controller/ProfileMenuTest.java
@@ -0,0 +1,15 @@
+package controller;
+
+import model.game.User;
+
+import java.io.ByteArrayOutputStream;
+import java.util.ArrayList;
+
+import static org.junit.Assert.*;
+
+public class ProfileMenuTest {
+    private ProfileMenu profileMenu;
+    private static final ArrayList<User> allUsersTemp = new ArrayList<>();
+    ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+
+}
