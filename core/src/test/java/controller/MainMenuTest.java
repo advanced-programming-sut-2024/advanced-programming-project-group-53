b8f5994b8639eddd93c80a9bbd01f78986a1b360
farbodft <farbod.ft2005@gmail.com>
2024-06-19 00:36:52 +0330

    temp for current User created

--- core/src/test/java/controller/MainMenuTest.java
+++ core/src/test/java/controller/MainMenuTest.java
@@ -14,6 +14,7 @@ import static org.junit.Assert.*;
 public class MainMenuTest {
     private MainMenu mainMenu;
     private static final ArrayList<User> allUsersTemp = new ArrayList<>();
+    private static final User currentUserTemp = User.getCurrentUser();
     ByteArrayOutputStream outContent = new ByteArrayOutputStream();
 
     @Before
