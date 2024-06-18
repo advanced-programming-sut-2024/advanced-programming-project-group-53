3b6cd5fb2a6880f44b51a500611c949fc3030471
farbodft <farbod.ft2005@gmail.com>
2024-06-19 00:10:47 +0330

    Main menu test created

---
+++ core/src/test/java/controller/MainMenuTest.java
@@ -0,0 +1,12 @@
+package controller;
+
+import java.io.ByteArrayOutputStream;
+
+import static org.junit.Assert.*;
+
+public class MainMenuTest {
+    private MainMenu mainMenu;
+    ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+
+
+}
