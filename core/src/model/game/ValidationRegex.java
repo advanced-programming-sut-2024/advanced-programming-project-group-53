5ecaebc2523bfdbbf550d1ce94f797650318b119
farbodft <farbod.ft2005@gmail.com>
2024-06-18 23:30:19 +0330

    validation Regex for weak password updated

--- core/src/model/game/ValidationRegex.java
+++ core/src/model/game/ValidationRegex.java
@@ -7,7 +7,7 @@ public enum ValidationRegex {
     Email(Pattern.compile("^(?<email>\\S+)@(yahoo|gmail|outlook|hotmail)\\.(com)$")),
     Username(Pattern.compile("^(?<username>([a-z]|[A-Z]|[0-9]|-)+)$")),
     Nickname(Pattern.compile("^(?<username>([a-z]|[A-Z]|[0-9]|-)+)$")),
-    Password(Pattern.compile("^(?=.*[A-Za-z])(?=.*\\d)(?=.*[@$!%*#?&])[A-Za-z\\d@$!%*#?&]{8,}$"));
+    Password(Pattern.compile("^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*#?&])[A-Za-z\\d@$!%*#?&]{8,}$"));
     private String value;
 
     private final Pattern pattern;
