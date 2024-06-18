f018f940c6d5e1a27dbe3d0a51827ce9bd720931
farbodft <farbod.ft2005@gmail.com>
2024-06-19 01:16:16 +0330

    set up method for before testing of profile menu added

--- core/src/test/java/controller/ProfileMenuTest.java
+++ core/src/test/java/controller/ProfileMenuTest.java
@@ -1,6 +1,7 @@
 package controller;
 
 import model.game.User;
+import org.junit.Before;
 
 import java.io.ByteArrayOutputStream;
 import java.util.ArrayList;
@@ -10,6 +11,15 @@ import static org.junit.Assert.*;
 public class ProfileMenuTest {
     private ProfileMenu profileMenu;
     private static final ArrayList<User> allUsersTemp = new ArrayList<>();
+    private static final User currentUserTemp = User.getCurrentUser();
     ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+
+    @Before
+    public void setUp() {
+        profileMenu = ProfileMenu.getInstance();
+        User.resetUsers(allUsersTemp);
+        User userInstance = new User("aValidUsername-1", "aValid-1Nickname", "valid2mail@gmail.com", "Valid#Strong45password");
+        User.setCurrentUser(userInstance);
+    }
 
 }
