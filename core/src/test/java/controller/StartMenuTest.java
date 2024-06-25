cc6c1fba6e11743d978da8066db1339a1523eacd
farbodft <farbod.ft2005@gmail.com>
2024-06-26 00:12:39 +0330

    setUp method for before tests created.

--- core/src/test/java/controller/StartMenuTest.java
+++ core/src/test/java/controller/StartMenuTest.java
@@ -1,6 +1,8 @@
 package controller;
 
 import model.game.User;
+import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 import java.io.ByteArrayOutputStream;
@@ -13,6 +15,14 @@ public class StartMenuTest {
     private static final ArrayList<User> allUsersTemp = new ArrayList<>();
     private static final User currentUserTemp = User.getCurrentUser();
     ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+
+    @BeforeClass
+    public static void setUp() {
+        User.reestUsers(allUsersTemp);
+        User userInstance1 = new User("aValidUsername-1", "aValid-1Nickname", "valid1mail@gmail.com", "Valid#Strong1password");
+        User userInstance2 = new User("aValidUsername-2", "aValid-2Nickname", "valid2mail@gmail.com", "Valid#Strong2password");
+        User.setCurrentUser(userInstance1);
+    }
 
 
 }
