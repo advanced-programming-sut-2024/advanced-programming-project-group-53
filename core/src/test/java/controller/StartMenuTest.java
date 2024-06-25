eaa6887b57188159b1f2e78c6398179ea674a4be
farbodft <farbod.ft2005@gmail.com>
2024-06-26 00:04:55 +0330

    test file for start menu created.

---
+++ core/src/test/java/controller/StartMenuTest.java
@@ -0,0 +1,18 @@
+package controller;
+
+import model.game.User;
+import org.junit.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.util.ArrayList;
+
+import static org.junit.Assert.*;
+
+public class StartMenuTest {
+    private StartMenu startMenu;
+    private static final ArrayList<User> allUsersTemp = new ArrayList<>();
+    private static final User currentUserTemp = User.getCurrentUser();
+    ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+
+
+}
