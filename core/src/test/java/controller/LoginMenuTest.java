7710c8bfcfe857cfddcc112d9bb3508096d47087
farbodft <farbod.ft2005@gmail.com>
2024-07-05 21:15:15 +0330

    new version of tests for LoginMenuTest.java added.

--- core/src/test/java/controller/LoginMenuTest.java
+++ core/src/test/java/controller/LoginMenuTest.java
@@ -1,5 +1,13 @@
 package controller;
 
+import model.game.User;
+import org.junit.Before;
+import org.junit.Test;
+import view.Message;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
 public class LoginMenuTest {
     /*private LoginMenu loginMenu;
     private static final ArrayList<User> allUsersTemp = new ArrayList<>();
@@ -90,4 +98,94 @@ public class LoginMenuTest {
     public void loadUsers() {
         User.loadUsers(allUsersTemp);
     }*/
+
+    private LoginMenu loginMenu;
+
+    @Before
+    public void setUp() {
+        loginMenu = LoginMenu.getInstance();
+    }
+
+    @Test
+    public void shouldErrorNoUser() {
+        if(User.findUser("Username") != null)
+            fail("test is invalid");
+        assertEquals(Message.NO_USER.message(),loginMenu.userValidation("Username"));
+        assertEquals(Message.NO_USER.message(),loginMenu.login("Username","Password"));
+    }
+
+    @Test
+    public void shouldReturnValidUser() {
+        if(User.findUser("Username") != null)
+            assertEquals("Username",loginMenu.userValidation("Username"));
+        else{
+            User user = new User("Username","Nickname","mail@gmail.com","Password123#","Question?","Answer");
+            assertEquals("Username",loginMenu.userValidation("Username"));
+            User.deleteAccount(user);
+        }
+    }
+
+    @Test
+    public void shouldErrorWrongPassword() {
+        if(User.findUser("Username") != null)
+            fail("test is invalid");
+        User user = new User("Username","Nickname","mail@gmail.com","Password123#","Question?","Answer");
+        assertEquals(Message.INCORRECT_PASSWORD.message(),loginMenu.login("Username","WrongPassword123"));
+        User.deleteAccount(user);
+    }
+
+    @Test
+    public void shouldLoginToUser() {
+        if(User.findUser("Username") != null)
+            fail("test is invalid");
+        User user = new User("Username","Nickname","mail@gmail.com","Password123#","Question?","Answer");
+        assertEquals("",loginMenu.login("Username","Password123#"));
+        User.deleteAccount(user);
+    }
+
+    @Test
+    public void shouldReturnQuestion() {
+        if(User.findUser("Username") != null)
+            fail("test is invalid");
+        User user = new User("Username","Nickname","mail@gmail.com","Password123#","Question?","Answer");
+        assertEquals("Question?",loginMenu.question("Username"));
+        User.deleteAccount(user);
+    }
+
+    @Test
+    public void shouldErrorWrongAnswer() {
+        if(User.findUser("Username") != null)
+            fail("test is invalid");
+        User user = new User("Username","Nickname","mail@gmail.com","Password123#","Question?","Answer");
+        assertEquals(Message.WRONG_ANSWER.message(),loginMenu.changePassword("WrongAnswer","NewPassword123#","NewPassword123#","Username"));
+        User.deleteAccount(user);
+    }
+
+    @Test
+    public void shouldErrorNotSameConfirmPassword() {
+        if(User.findUser("Username") != null)
+            fail("test is invalid");
+        User user = new User("Username","Nickname","mail@gmail.com","Password123#","Question?","Answer");
+        assertEquals(Message.PASSWORD_IS_NOT_THE_SAME.message(),loginMenu.changePassword("Answer","NewPassword123#","NewPassword13#","Username"));
+        User.deleteAccount(user);
+    }
+
+    @Test
+    public void shouldErrorWeakNewPass() {
+        if(User.findUser("Username") != null)
+            fail("test is invalid");
+        User user = new User("Username","Nickname","mail@gmail.com","Password123#","Question?","Answer");
+        assertEquals(Message.WEAK_PASSWORD.message()+"\n",loginMenu.changePassword("Answer","NewPassword","NewPassword","Username"));
+        User.deleteAccount(user);
+    }
+
+    @Test
+    public void shouldChangePassword() {
+        if(User.findUser("Username") != null)
+            fail("test is invalid");
+        User user = new User("Username","Nickname","mail@gmail.com","Password123#","Question?","Answer");
+        assertEquals(Message.PASSWORD_CHANGED.message(),loginMenu.changePassword("Answer","NewPassword123#","NewPassword123#","Username"));
+        assertEquals("NewPassword123#",user.password());
+        User.deleteAccount(user);
+    }
 }
