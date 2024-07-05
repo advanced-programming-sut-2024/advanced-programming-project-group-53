0661eec0179e3c1c941384b325fb0596a3607352
farbodft <farbod.ft2005@gmail.com>
2024-07-05 22:11:36 +0330

    LoginMenuTest.java tests updated based on latest version of project.

--- core/src/test/java/controller/LoginMenuTest.java
+++ core/src/test/java/controller/LoginMenuTest.java
@@ -9,96 +9,6 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.fail;
 
 public class LoginMenuTest {
-    /*private LoginMenu loginMenu;
-    private static final ArrayList<User> allUsersTemp = new ArrayList<>();
-
-    @Before
-    public void setUp() {
-        loginMenu = LoginMenu.getInstance();
-        User.resetUsers(allUsersTemp);
-        User userInstance = new User("ValidUsername", "ValidNickname", "Validemail@yahoo.com", "ValidAndStrongPassword12$$");
-        //Todo: set some answers for security questions.
-    }
-
-    @Test
-    public void shouldEnterMainMenu() {
-        ByteArrayOutputStream outContent = new ByteArrayOutputStream();
-        System.setOut(new PrintStream(outContent));
-        assertTrue(loginMenu.enterMenu("MainMenu"));
-        assertEquals(Message.ENTER_MAIN_MENU.message(), outContent.toString().trim());
-    }
-
-    @Test
-    public void shouldShowLoginMenuName() {
-        ByteArrayOutputStream outContent = new ByteArrayOutputStream();
-        System.setOut(new PrintStream(outContent));
-        loginMenu.showMenu();
-        assertEquals(Message.LOGIN_MENU.message(), outContent.toString().trim());
-    }
-
-    @Test
-    public void shouldShowInvalidMenu() {
-        ByteArrayOutputStream outContent = new ByteArrayOutputStream();
-        System.setOut(new PrintStream(outContent));
-        assertFalse(loginMenu.enterMenu("aMenu"));
-        assertEquals(Message.INVALID_MENU.message(), outContent.toString().trim());
-    }
-
-    @Test
-    public void shouldLoginValidAccount() {
-        assertTrue(loginMenu.login("ValidUsername", "ValidAndStrongPassword12$$"));
-    }
-
-    @Test
-    public void shouldErrorNoUser() {
-        ByteArrayOutputStream outContent = new ByteArrayOutputStream();
-        System.setOut(new PrintStream(outContent));
-        assertFalse(loginMenu.login("ANotExistingUsername", "APassword"));
-        assertEquals(Message.NO_USER.message(), outContent.toString().trim());
-    }
-
-    @Test
-    public void shouldErrorIncorrectPassword() {
-        ByteArrayOutputStream outContent = new ByteArrayOutputStream();
-        System.setOut(new PrintStream(outContent));
-        assertFalse(loginMenu.login("ValidUsername", "AWrongPassword"));
-        assertEquals(Message.INCORRECT_PASSWORD.message(), outContent.toString().trim());
-    }
-
-    @Test
-    public void shouldValidateUsernameForgottenPassword() {
-        assertTrue(loginMenu.forgetPasswordUserValidation("ValidUsername"));
-    }
-
-    @Test
-    public void shouldErrorInvalidUsernameForgottenPassword() {
-        assertFalse(loginMenu.forgetPasswordUserValidation("AnInvalidUsername"));
-    }
-
-    @Test
-    public void shouldChangePasswordWithStrongPassword() {
-        ByteArrayOutputStream outContent = new ByteArrayOutputStream();
-        System.setOut(new PrintStream(outContent));
-        loginMenu.setPassword("AnotherValidAndStrongPassword12$$", "ValidUsername");
-        assertEquals(Message.PASSWORD_CHANGED.message(), outContent.toString().trim());
-        //Todo: check that the password has actually changed or not
-    }
-
-    @Test
-    public void shouldErrorWeakPassword() {
-        ByteArrayOutputStream outContent = new ByteArrayOutputStream();
-        System.setOut(new PrintStream(outContent));
-        loginMenu.setPassword("WeakPass", "ValidUsername");
-        assertEquals(Message.WEAK_PASSWORD.message(), outContent.toString().trim());
-        //Todo: check that the password hasn't changed
-    }
-    //todo: tests for security question
-
-    @After
-    public void loadUsers() {
-        User.loadUsers(allUsersTemp);
-    }*/
-
     private LoginMenu loginMenu;
 
     @Before
@@ -120,7 +30,7 @@ public class LoginMenuTest {
             assertEquals("Username",loginMenu.userValidation("Username"));
         else{
             User user = new User("Username","Nickname","mail@gmail.com","Password123#","Question?","Answer");
-            assertEquals("Username",loginMenu.userValidation("Username"));
+            assertEquals("empty",loginMenu.userValidation("Username"));
             User.deleteAccount(user);
         }
     }
@@ -139,7 +49,7 @@ public class LoginMenuTest {
         if(User.findUser("Username") != null)
             fail("test is invalid");
         User user = new User("Username","Nickname","mail@gmail.com","Password123#","Question?","Answer");
-        assertEquals("",loginMenu.login("Username","Password123#"));
+        assertEquals("empty",loginMenu.login("Username","Password123#"));
         User.deleteAccount(user);
     }
 
@@ -175,7 +85,7 @@ public class LoginMenuTest {
         if(User.findUser("Username") != null)
             fail("test is invalid");
         User user = new User("Username","Nickname","mail@gmail.com","Password123#","Question?","Answer");
-        assertEquals(Message.WEAK_PASSWORD.message()+"\n",loginMenu.changePassword("Answer","NewPassword","NewPassword","Username"));
+        assertEquals(Message.WEAK_PASSWORD.message(),loginMenu.changePassword("Answer","NewPassword","NewPassword","Username"));
         User.deleteAccount(user);
     }
 
