00134682699808dfa074fca03117c551b82a67f7
farbodft <farbod.ft2005@gmail.com>
2024-07-07 23:00:05 +0330

    before and after methods for test class of ConnectorTest.java added.

---
+++ core/src/test/java/network/ConnectorTest.java
@@ -0,0 +1,58 @@
+package network;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.BufferedInputStream;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+import static org.junit.Assert.*;
+
+public class ConnectorTest {
+    private Connector connector = new Connector();
+    private static final int port = 8080;
+    private ServerSocket serverSocket;
+    private Thread serverThread;
+    private boolean running;
+
+    @Before
+    public void setUp() throws IOException {
+        serverSocket = new ServerSocket(port);
+        running = true;
+        serverThread = new Thread(() -> {
+            try {
+                while (running) {
+                    Socket clientSocket = serverSocket.accept();
+                    DataInputStream dis = new DataInputStream(new BufferedInputStream(clientSocket.getInputStream()));
+                    DataOutputStream dos = new DataOutputStream(clientSocket.getOutputStream());
+
+                    String received = dis.readUTF();
+                    Instruction instruction = Instruction.fromString(received);
+
+                    dos.writeUTF(instruction.toString());//just echoing back the same instruction to be simple
+
+                    dis.close();
+                    dos.close();
+                    clientSocket.close();
+                }
+            } catch (IOException e) {
+                if (running) {
+                    e.printStackTrace();
+                }
+            }
+        });
+        serverThread.start();
+    }
+
+    @After
+    public void end() throws IOException {
+        running = false;
+        serverSocket.close();
+    }
+
+}
