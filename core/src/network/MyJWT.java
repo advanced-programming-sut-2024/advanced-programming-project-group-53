1ade2998a211b8d3ae5dc409205ba42ea7f8faa8
Kiarash Sanei <kiarashsanei5@gmail.com>
2024-07-08 23:05:09 +0330

    Adding dependency.

---
+++ core/src/network/MyJWT.java
@@ -0,0 +1,34 @@
+package network;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.interfaces.DecodedJWT;
+import com.auth0.jwt.interfaces.JWTVerifier;
+
+import java.util.Date;
+
+public class MyJWT {
+    private static final String SECRET_KEY = "mySecretKey";
+    private static final long EXPIRATION_TIME = 15 * 60 * 1000;
+
+    public static String generateToken(String subject) {
+        Algorithm algorithm = Algorithm.HMAC256(SECRET_KEY);
+        return JWT.create()
+                .withSubject(subject)
+                .withIssuedAt(new Date())
+                .withExpiresAt(new Date(System.currentTimeMillis() + EXPIRATION_TIME))
+                .sign(algorithm);
+    }
+
+    public static boolean validateToken(String token) {
+        try {
+            Algorithm algorithm = Algorithm.HMAC256(SECRET_KEY);
+            JWTVerifier verifier = JWT.require(algorithm).build();
+            DecodedJWT jwt = verifier.verify(token);
+            return !jwt.getExpiresAt().before(new Date());
+        } catch (Exception exception) {
+            exception.printStackTrace();
+            return false;
+        }
+    }
+}
