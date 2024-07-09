a62c69539fd9f45bcfabbffaf817776db30ea830
farbodft <farbod.ft2005@gmail.com>
2024-07-09 12:53:47 +0330

    test for adding players created.

--- core/src/test/java/controller/TournamentMenuTest.java
+++ core/src/test/java/controller/TournamentMenuTest.java
@@ -14,5 +14,10 @@ public class TournamentMenuTest {
         tournamentMenu = TournamentMenu.getInstance();
     }
 
-
+    @Test
+    public void shouldAddPlayers() {
+        tournamentMenu.addPlayer("player1", "player2", "player3", "player4", "player5", "player6", "player7", "player8");
+        assertEquals(1, tournamentMenu.getGames().size());
+        assertArrayEquals(new String[]{"player1", "player2", "player3", "player4", "player5", "player6", "player7", "player8"}, tournamentMenu.getGames().get(0));
+    }
 }
