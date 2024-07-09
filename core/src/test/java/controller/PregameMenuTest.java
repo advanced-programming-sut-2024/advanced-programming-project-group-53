a2172ec93ceda6613f87c3d943080e715e8dc7ac
farbodft <farbod.ft2005@gmail.com>
2024-07-09 19:05:20 +0330

    base for PregameMenuTest.java created.

---
+++ core/src/test/java/controller/PregameMenuTest.java
@@ -0,0 +1,21 @@
+package controller;
+
+import game.GWENT;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class PregameMenuTest {
+
+    private PregameMenu pregameMenu;
+    private GWENT game;
+
+
+    @Before
+    public void setUp() {
+        pregameMenu = PregameMenu.getInstance();
+    }
+
+
+}
