a1ff49e5310667f09eba1b962fa8d4e3ee15d3df
farbodft <farbod.ft2005@gmail.com>
2024-07-09 19:14:15 +0330

    tests for adding users to waiting list added.

--- core/src/test/java/controller/PregameMenuTest.java
+++ core/src/test/java/controller/PregameMenuTest.java
@@ -1,6 +1,7 @@
 package controller;
 
 import game.GWENT;
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -17,5 +18,24 @@ public class PregameMenuTest {
         pregameMenu = PregameMenu.getInstance();
     }
 
+    @Test
+    public void shouldAddOneUserToWaiting() {
+        pregameMenu.addToWaiting("Username1", game);
+        assertEquals(1, pregameMenu.getWaiting().size());
+        assertEquals("Username1", pregameMenu.getWaiting().get(0));
+    }
 
+    @Test
+    public void shouldAddTwoUsersToWaiting() {
+        pregameMenu.addToWaiting("Username1", game);
+        pregameMenu.addToWaiting("Username2", game);
+        //after adding two players the waiting list should empty
+        assertEquals(0, pregameMenu.getWaiting().size());
+    }
+
+    @After
+    public void end() {
+        //to clear waiting list after each test
+        pregameMenu.getWaiting().clear();
+    }
 }
