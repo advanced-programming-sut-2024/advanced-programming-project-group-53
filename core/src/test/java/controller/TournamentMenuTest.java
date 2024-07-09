d709a6e3f45ea90a85302a480ca5b5702fe81d3c
farbodft <farbod.ft2005@gmail.com>
2024-07-09 12:47:30 +0330

    base for TournamentMenuTest.java created.

---
+++ core/src/test/java/controller/TournamentMenuTest.java
@@ -0,0 +1,18 @@
+package controller;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class TournamentMenuTest {
+
+    private TournamentMenu tournamentMenu;
+
+    @Before
+    public void setUp() {
+        tournamentMenu = TournamentMenu.getInstance();
+    }
+
+
+}
